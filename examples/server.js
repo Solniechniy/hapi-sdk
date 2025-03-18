662bf2228e9123b66b34bb483c662b7d96fadd07
Solniechniy <oleksndrkorn@gmail.com>
2025-03-18 13:05:05 +0200

    Add HTML integration examples and documentation

--- examples/server.js
+++ examples/server.js
@@ -1,14 +1,25 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Serve static files from the examples directory
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname)));
 
-// Serve the SDK bundle
-app.use('/sdk', express.static(path.join(__dirname, '../dist')));
+// Serve the node_modules directory for direct access to the npm package
+app.use(
+  "/node_modules",
+  express.static(path.join(__dirname, "../node_modules"))
+);
 
-const PORT = process.env.PORT || 3000;
+// Serve the index.html file
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Start the server
 app.listen(PORT, () => {
-    console.log(`Test server running on http://localhost:${PORT}`);
-}); 
+  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Open your browser to test the SDK`);
+});
